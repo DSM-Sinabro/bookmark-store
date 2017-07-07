e8b67a1654763bda4ea7174588d15f0a653c7f22
PRKM <dudgns2486@gmail.com>
2017-07-07 17:06:46 +0900

    Main page updated

---
+++ public/js/searchbar.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('select').material_select();
+  $('.chips').material_chip();
+
+  $('#recommandButton').on('click', function(event){
+    var target = $(event.target);
+    target.toggleClass('grey-text lignten-2');
+    target.toggleClass('red-text');
+    if(target.hasClass('red-text')){
+      var nextCount = parseInt(target.siblings('#recommandCount').html()) + 1;
+      target.siblings('#recommandCount').html(nextCount);
+    }else{
+      var nextCount = parseInt(target.siblings('#recommandCount').html()) - 1;
+      target.siblings('#recommandCount').html(nextCount);
+    }
+  })
+});
