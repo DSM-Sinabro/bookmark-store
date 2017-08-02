db8d60fe3f684c1bd407f54c5728425d77ad0fa8
PRKM <dudgns2486@gmail.com>
2017-08-02 21:05:11 +0900

    Update Login&Register Page etc.
    detail
    - Moved Login&Register Page
    - Added javascript code in main.html

--- public/js/searchbar.js
+++ public/js/searchbar.js
@@ -9,14 +9,44 @@ $(document).ready(function() {
 
   $('#recommandButton').on('click', function(event){
     var target = $(event.target);
-    target.toggleClass('grey-text lignten-2');
-    target.toggleClass('red-text');
-    if(target.hasClass('red-text')){
-      var nextCount = parseInt(target.siblings('#recommandCount').html()) + 1;
-      target.siblings('#recommandCount').html(nextCount);
-    }else{
-      var nextCount = parseInt(target.siblings('#recommandCount').html()) - 1;
-      target.siblings('#recommandCount').html(nextCount);
-    }
+
+    var bookmarkId = target.parents('.card').find('input[type="hidden"]').val();
+
+    $.ajax({
+      url: 'bookmarks',
+      type: 'PATCH',
+      data: {bookmarkId: bookmarkId},
+      success: function(result) {
+        target.toggleClass('grey-text lignten-2');
+        target.toggleClass('red-text');
+        if(result == 'plus'){
+          var nextCount = parseInt(target.siblings('#recommandCount').html()) + 1;
+        }else{
+          var nextCount = parseInt(target.siblings('#recommandCount').html()) - 1;
+        }
+        target.siblings('#recommandCount').html(nextCount);
+      },
+      error: function(xhr, status) {
+        alert('error occurred!!');
+      }
+    });
+  });
+
+  $('#searchButton').click(function(){
+    var data = $('#searchForm').serialize();
+    data += '&tags=' + JSON.stringify($('#searchtags').material_chip('data'));
+    console.log(data);
+
+    $.ajax({
+      url: './bookmarks',
+      type: 'GET',
+      data: data,
+      success: function(result){
+        // TODO 검색결과 삽입
+      },
+      error: function(xhr, status){
+        alert('error occurred!!');
+      }
+    })
   })
 });
